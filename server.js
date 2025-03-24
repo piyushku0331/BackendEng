08db77fe1af0c8bd629d818643255edba6d34397
Piyushku0331 <Piyushku0331@gmail.com>
2025-03-24 18:15:15 +0530

    added home page

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const user = require('./model/user');
+const blog = require('./model/blog');
+
 const app = express();
 
 const PORT = 5000;
+
+app.get('/',(req,res)=>{
+    res.status(200).send("Hello Welcome to Home Page");
+})
 
 mongoose.connect('mongodb://localhost:27017/Mongoose-1')
 .then(()=>console.log("Connected!!"))
