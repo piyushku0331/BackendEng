1259512256829bb5f05a0740dd96dc436044e458
Piyushku0331 <Piyushku0331@gmail.com>
2025-03-26 16:57:56 +0530

    new commit

--- server.js
+++ server.js
@@ -8,6 +8,28 @@ const app = express();
 
 const PORT = 5000;
 
+app.post('/users',async(req,res)=>{
+    let {name,email,password} = req.body;
+    let newUser = new user({
+        name:name,
+        email:email,
+        password:password
+    });
+    await newUser.save();
+    res.send("User added");
+})
+
+app.get('/users',async(req,res)=>{
+    let allusers = await user.find();
+    res.send(allusers);
+})
+
+app.get('/users/:id',async (req,res)=>{
+    let {id} = req.params;
+    let oneUser = user.findById(id);
+    res.send(oneUser);
+})
+
 app.get('/',(req,res)=>{
     res.status(200).send("Hello Welcome to Home Page");
 })
