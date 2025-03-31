e58f0c2acd2bc7a7d4bd15435af24edbc0b200b6
Piyushku0331 <Piyushku0331@gmail.com>
2025-03-31 17:22:25 +0530

    added blog routes

--- server.js
+++ server.js
@@ -44,16 +44,50 @@ app.put('/users/:id',async (req,res)=>{
     let {id} = req.params;
     let {name,email,password} = req.body;
     let updateUser = await user.findById(id);
-    // {
-    //     name:"Piyush",
-    //     email:"Piyushku0331@gmail.com",
-    //     password:"123456"
-    // }
     updateUser.name=name;
     updateUser.email=email;
     updateUser.password=password;
     await updateUser.save();
     res.send("User Updated");
+});
+
+app.post('/blogs',async(req,res)=>{
+    let {content,author,date} = req.body;
+    let newBlog = new blog({
+        content:content,
+        author:author,
+        date:date
+    });
+    await newBlog.save();
+    res.send("Blog added");
+});
+
+app.get('/blogs',async(req,res)=>{
+    let allblogs = await blog.find();
+    res.send(allblogs);
+});
+
+app.get('/blogs/:id',async (req,res)=>{
+    let {id} = req.params;
+    let oneBlog = blog.findById(id);
+    res.send(oneBlog);
+});
+
+app.delete('/deleteoneBlog/:id',async (req,res)=>{
+    let {id} = req.params;
+    await Blog.findByIdAndDelete(id);
+    res.send("Blog Deleted");
+});
+
+app.put('/blogs/:id',async (req,res)=>{
+    let {id} = req.params;
+    let {content,author,date} = req.body;
+    let updateBlog = await blog.findById(id);
+    updateBlog.content=content;
+    updateBlog.author=author;
+    updateBlog.date=date;
+    await updateBlog.save();
+    res.send("Blog Updated");
 });
 
 mongoose.connect('mongodb://localhost:27017/Mongoose-1')
